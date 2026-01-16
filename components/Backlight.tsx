0e1a6c9c5c131ef64796f10b90fa224e00f8718d
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2026-01-16 17:31:53 +0300

    fix(backlight): telegram color

--- components/Backlight.tsx
+++ components/Backlight.tsx
@@ -1,3 +1,5 @@
+import { useSiteType } from "./SiteTypeContext";
+
 interface BacklightProps {
     count: number;
     gap?: number;
@@ -7,9 +9,10 @@ interface BacklightProps {
 }
 
 export default function Backlight({ count, gap=10, fromColor, toColor, marginTop }:BacklightProps) {
+    const { siteType } = useSiteType()
     return (
         <div className={`absolute justify-center mt-35 w-[75%] top-0 left-[50%] -translate-x-[50%] flex flex-col blur-lg`} style={{ gap: gap }} >
-            {Array(count).fill(1).map((_, i)=><div key={i} className="w-full h-76 rounded-full bg-radial from-[rgba(70,249,215,1)] to-[rgba(21,181,237,0)] opacity-20 blur-3xl" style={{"--tw-gradient-from": fromColor, "--tw-gradient-to": toColor, marginTop: marginTop} as React.CSSProperties} />)}
+            {Array(count).fill(1).map((_, i)=><div key={i} className={`w-full h-76 rounded-full bg-radial ${siteType === "game" ? "from-[rgba(70,249,215,1)]" : "from-[#15B5ED]"} to-[rgba(21,181,237,0)] opacity-20 blur-3xl`} style={{"--tw-gradient-from": fromColor, "--tw-gradient-to": toColor, marginTop: marginTop} as React.CSSProperties} />)}
         </div>
     )
 }
